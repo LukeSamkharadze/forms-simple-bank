e99bae25d939691c1dff052b3e5430a89331f51b
Luke Samkharadze <samkharadzeluke@gmail.com>
2018-07-26 17:31:05 +0400

    Add project files.

---
+++ BankPCL/Implementations/Strategies/Account Strategies/AccountGreenStateStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BankPCL.Abstractions.Interfaces.Strategies;
+
+namespace BankPCL.Implementations.Strategies.Account_Strategies
+{
+    public class AccountGreenStateStrategy
+    {
+        public double Limit { get; set; } = 10000;
+        public double Rating { get; set; }
+
+        public double WithdrawUsingRating(double amount)
+        {
+            if (amount < Limit * Rating / 10)
+            {
+                return amount;
+            }
+
+            throw new Exception("Can't WithdrawUsingRating");
+        }
+    }
+}
---
+++ BankPCL/Implementations/Strategies/Account Strategies/AccountRedStateStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BankPCL.Abstractions.Interfaces.Strategies;
+
+namespace BankPCL.Implementations.Strategies.Account_Strategies
+{
+    public class AccountRedStateStrategy
+    {
+        public double? Limit { get; set; }
+        public double? Rating { get; set; }
+
+    }
+}
