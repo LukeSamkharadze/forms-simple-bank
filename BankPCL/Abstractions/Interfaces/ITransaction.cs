8e82eb82c9bfee1938a5da089a62d16d0da08931
Luke Samkharadze <samkharadzeluke@gmail.com>
2018-08-03 14:44:09 +0400

    Added ITransaction and Transaction Interface and Class

---
+++ BankPCL/Abstractions/Interfaces/ITransaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankPCL.Abstractions.Interfaces
+{
+    public interface ITransaction
+    {
+        IAccount FromAccount { get; set; }
+        IAccount ToAccount { get; set; }
+
+        double? amount { get; set; }
+    }
+}
---
+++ BankPCL/Implementations/Classes/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using BankPCL.Abstractions.Interfaces;
+
+namespace BankPCL.Implementations.Classes
+{
+    public class Transaction : ITransaction
+    {
+        public IAccount FromAccount { get; set; }
+        public IAccount ToAccount { get; set; }
+
+        public double? amount { get; set; }
+    }
+}
